6deaaf5b0335b3cf02c99a6738474a52551afc1f
Diego Nascimento de Oliveira <138056676+diegonascimento2023@users.noreply.github.com>
2024-09-26 10:16:10 +0000

    :sparkles: feat: Função inserir_conta

--- contaBancaria.c
+++ contaBancaria.c
@@ -22,3 +22,33 @@ ContaBancaria *criar_conta(int numero, int agenciaNumero, const char *nomeClient
     novaConta->direita = NULL;
     return novaConta;
 }
+
+
+void inserir_conta(ContaBancaria **raiz, ContaBancaria *novaConta)
+{
+    // Verifica se novaConta é NULL
+    if (novaConta == NULL)
+    {
+        printf("Erro: nova conta é NULL.\n");
+        return; // Sai da função se novaConta for NULL
+    }
+
+    // Inserção da nova conta
+    if (*raiz == NULL)
+    {
+        *raiz = novaConta; // Se a raiz é NULL, insere a nova conta aqui
+    }
+    else if (novaConta->numero < (*raiz)->numero)
+    {
+        inserir_conta(&(*raiz)->esquerda, novaConta); // Insere na subárvore esquerda
+    }
+    else if (novaConta->numero > (*raiz)->numero)
+    {
+        inserir_conta(&(*raiz)->direita, novaConta); // Insere na subárvore direita
+    }
+    else
+    {
+        printf("A conta com numero %d ja existe.\n", novaConta->numero); // Conta já existente
+        free(novaConta);                                                 // Libera a memória da nova conta
+    }
+}
