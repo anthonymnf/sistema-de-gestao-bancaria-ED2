b9894f3d50ef4d86cdb58caa4efb069d7f8c2815
Diego Nascimento de Oliveira <138056676+diegonascimento2023@users.noreply.github.com>
2024-09-26 10:11:31 +0000

    :sparkles: featt: Adicionando função criar_conta

--- contaBancaria.c
+++ contaBancaria.c
@@ -3,3 +3,22 @@
 #include <stdlib.h>
 #include <string.h>
 #include "contaBancaria.h"
+
+ContaBancaria *criar_conta(int numero, int agenciaNumero, const char *nomeCliente, const char *dataAbertura, float saldo, const char *status)
+{
+    ContaBancaria *novaConta = (ContaBancaria *)malloc(sizeof(ContaBancaria));
+    if (novaConta == NULL)
+    {
+        printf("Erro ao alocar memoria para nova conta.\n");
+        return NULL;
+    }
+    novaConta->numero = numero;
+    novaConta->agenciaNumero = agenciaNumero;
+    strcpy(novaConta->nomeCliente, nomeCliente);
+    strcpy(novaConta->dataAbertura, dataAbertura);
+    novaConta->saldo = saldo;
+    strcpy(novaConta->status, status);
+    novaConta->esquerda = NULL;
+    novaConta->direita = NULL;
+    return novaConta;
+}
